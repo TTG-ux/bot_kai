85597b5e2f54b5924fb9d548709204d3741cdab4
TTG-ux <73063341+TTG-ux@users.noreply.github.com>
2021-05-14 21:06:12 +0300

    Create config.py

---
+++ data/config.py
@@ -0,0 +1,20 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Заберем токен нашего бота (прописать в файле ".env")
+BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
+
+admins = [
+    os.getenv("ADMIN_ID"),
+]
+
+host = str(os.getenv("PG_HOST"))
+
+PGUSER = str(os.getenv("PG_USER"))
+
+password = str(os.getenv("PG_PASS"))
+
+
