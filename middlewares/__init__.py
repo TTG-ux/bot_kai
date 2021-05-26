53681d2bda2dfffacbc7918d3604e5482e9fc2d3
TTG-ux <73063341+TTG-ux@users.noreply.github.com>
2021-05-26 03:39:36 +0300

    Update __init__.py

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,7 +1,10 @@
 from aiogram import Dispatcher
 
+from loader import dp
+
+# from .throttling import ThrottlingMiddleware
 from .throttling import ThrottlingMiddleware
 
-
-def setup(dp: Dispatcher):
+if __name__ == '__main__':
+    # dp.middleware.setup(ThrottlingMiddleware())
     dp.middleware.setup(ThrottlingMiddleware())
