10ffb9b888b82a5b344b057408bc53ed3fc9eaf5
TTG-ux <73063341+TTG-ux@users.noreply.github.com>
2021-05-26 03:38:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
+from aiogram import executor
+
+from loader import dp
+import middlewares, filters, handlers
+from utils.notify_admins import on_startup_notify
 
 
-async def on_startup(dp):
-    import filters
-    import middlewares
-    filters.setup(dp)
-    middlewares.setup(dp)
-
-    from utils.notify_admins import on_startup_notify
-    await on_startup_notify(dp)
+async def on_startup(dispatcher):
+    await on_startup_notify(dispatcher)
 
 
 if __name__ == '__main__':
-    from aiogram import executor
-    from handlers import dp
-
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
