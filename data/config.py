8a21b2e8c054e2060ac1463939662e2675c820fd
TTG-ux <73063341+TTG-ux@users.noreply.github.com>
2021-05-27 21:22:36 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -11,12 +11,24 @@ admins = [
     os.getenv("ADMIN_ID"),
 ]
 
+database = str(os.getenv("Database"))
+
 host = str(os.getenv("PG_HOST"))
 
 PGUSER = str(os.getenv("PG_USER"))
 
 password = str(os.getenv("PG_PASS"))
 
+db_user = str(os.getenv("DB_USER"))
+
 banned_users = [123456, 12345666]
 
+POSTGRES_URI = f"postgresql://{PGUSER}:{password}@{host}/{database}"
+aiogram_redis = {
+    'host': host,
+}
 
+redis = {
+    'address': (host, 5432),
+    'encoding': 'utf8'
+}
