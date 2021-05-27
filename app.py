79a860e6f818f36b9573f8179d5951aec49f86e6
TTG-ux <73063341+TTG-ux@users.noreply.github.com>
2021-05-27 21:22:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ from aiogram import executor
 from loader import dp
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
+from utils.dp_api.database import create_db
+
+
 
 
 async def on_startup(dispatcher):
+    middlewares.setup(dispatcher)
+    handlers.users.setup(dispatcher)
+
     await on_startup_notify(dispatcher)
+    await create_db()
 
 
 if __name__ == '__main__':
