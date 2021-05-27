5708d21c23fd08757b11206d920f6d06e4319c01
TTG-ux <73063341+TTG-ux@users.noreply.github.com>
2021-05-27 21:22:32 +0300

    Update __init__.py

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,2 +1,12 @@
 from .start import dp
 from .purchase import dp
+import logging
+
+from .start import bot_start
+
+from aiogram import Dispatcher, filters
+
+
+def setup(dp: Dispatcher):
+    dp.register_message_handler(bot_start, filters.CommandStart())
+    logging.info("Handlers are successfully configured")
