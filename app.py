81af08a02b3c656d3d3a4d7b4c7c494c16321b33
TTG-ux <73063341+TTG-ux@users.noreply.github.com>
2021-06-07 20:52:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from aiogram import executor
 from loader import dp
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
-from utils.dp_api.database import create_db
 
 
 
@@ -13,8 +12,8 @@ async def on_startup(dispatcher):
     handlers.users.setup(dispatcher)
 
     await on_startup_notify(dispatcher)
-    await create_db()
+
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True,  on_startup=on_startup)
