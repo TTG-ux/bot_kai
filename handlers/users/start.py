dfdb77dcd24e3b53a32f92c1a186be70e58e25ed
TTG-ux <73063341+TTG-ux@users.noreply.github.com>
2021-06-07 20:52:27 +0300

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,28 +1,22 @@
 from aiogram import types, bot
 from aiogram.dispatcher.filters.builtin import CommandStart
-from data.config import admins
-
-from utils.dp_api import database
-
-from loader import dp
+from loader import dp, db
 from keyboards.inline.choice_buttons import determined_button
-from aiogram.types import (Message, InlineKeyboardMarkup, InlineKeyboardButton,
-                           CallbackQuery, LabeledPrice, PreCheckoutQuery)
-
-db = database.DBCommands()
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(m: types.Message):
-    chat_id = m.from_user.id
-    referral = m.get_args()
-    user = await db.add_new_user(referral=referral)
-    id = user.id
-    count_users = await db.count_users()
+    if (not db.subscriber_exists(m.from_user.id)):
+        # если юзера нет в базе, добавляем его
+        db.add_subscriber(m.from_user.id)
+    else:
+        # если он уже есть, то просто обновляем ему статус подписки
+        db.update_subscription(m.from_user.id, True)
+
     await m.answer(f"Привет, {m.from_user.full_name}!\n"
-                   f"Твоему вниманию представлено главное меню🎩\n\n"
-                   f"Высшее - Университет КНИТУ-КАИ и его факультеты(бакалвр/специалитет)\n\n"
-                   f"Среднее - Колледжи при КНИТУ-КАИ их специальности\n\n"
-                   f"В пункте (Часто задаваемые вопросы) ты можешь задать мне вопрос👱‍♀️\n\n"
+                   f"Твоему вниманию представлено <ins><b>Главное меню</b></ins>🎩\n\n"
+                   f"<em>Высшее</em> - <b>Университет КНИТУ-КАИ и его факультеты</b> (бакалавр/специалитет)\n\n"
+                   f"<em>Среднее</em> - <b>Колледжи при КНИТУ-КАИ их специальности</b>\n\n"
+                   f"В пункте (<ins>❓Часто задаваемые вопросы❓</ins>) ты можешь задать мне вопрос👱‍♀️\n\n"
                    f"Дальнейшие пункты хранят в себя важные ссылки/контакты🗣👀",
-                   reply_markup = determined_button)
+                   reply_markup=determined_button)
