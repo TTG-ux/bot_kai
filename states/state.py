1a9b489163df9def889d8195b3162f8a6bbf301e
TTG-ux <73063341+TTG-ux@users.noreply.github.com>
2021-06-07 20:52:07 +0300

    Update state.py

--- states/state.py
+++ states/state.py
@@ -4,3 +4,16 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 class Mailing(StatesGroup):
     Text = State()
     LVL1 = State()
+
+
+class Careitems(StatesGroup):
+    item1 = State()
+
+
+class Careitems_1(StatesGroup):
+    itemi1 = State()
+
+
+class text_one(StatesGroup):
+    texit = State()
+    texup = State()
