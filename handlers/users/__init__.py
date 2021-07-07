004dbb94e0e58d53ce35ea50b05a85d9783336dd
TTG-ux <73063341+TTG-ux@users.noreply.github.com>
2021-07-07 18:11:48 +0300

    Update __init__.py

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -2,6 +2,13 @@ from .start import dp
 from .start import bot_start
 import logging
 from .purchase import dp
+from . import Feedback
+from . import mat_ikt_menu
+from . import mat_khem_menu
+from . import mat_sosity_menu
+from . import Bachelor_course_main
+from . import iktfiz_degree_manu
+from . import Master_degree_program
 
 
 from aiogram import Dispatcher, filters
