c874a2a2b9b46ef4756ec7f6e181828bf9ae0456
TTG-ux <73063341+TTG-ux@users.noreply.github.com>
2024-07-31 22:52:57 +0300

    config.py

--- data/config.py
+++ data/config.py
@@ -7,8 +7,8 @@ load_dotenv()
 # Заберем токен нашего бота (прописать в файле ".env")
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
 
-admin = [771224251]
+admin = []
 
-admins = [771224251, 697432821, 371765926]
+admins = []
 
-buns = [1193572137]
+buns = []
